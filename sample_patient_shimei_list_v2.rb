a8ba0bbdeee8b56ada5b5dd7b34b3c41cc0f093b
ogawayuta <Og123017@gmail.com>
2014-09-03 10:32:41 +0900

    add file

--- sample_patient_shimei_list_v2.rb
+++ sample_patient_shimei_list_v2.rb
@@ -17,7 +17,7 @@ PASSWD = "ormaster123"
 CONTENT_TYPE = "application/xml"
 
 
-req = Net::HTTP::Post.new("/api01rv2/patientlst1v2?class=01")
+req = Net::HTTP::Post.new("/api01rv2/patientlst3v2?class=01")
 # class :01 新規・更新対象
 # class :02 新規対象
 
@@ -25,42 +25,39 @@ name = ARGV[0]
 puts "氏名検索:#{name}"
 puts "=========="
 puts "検索結果"
-puts 
+puts "=========="
+
 BODY = <<EOF
 <data>
-        <patientlst1req type="record">
-                <Base_StartDate type="string">2000-01-01</Base_StartDate>
-                <Base_EndDate type="string">2014-09-01</Base_EndDate>
-                <Contain_TestPatient_Flag type="string">1</Contain_TestPatient_Flag>
-        </patientlst1req>
+        <patientlst3req type="record">
+                <WholeName type="string">#{name}</WholeName>
+        </patientlst3req>
 </data>
 EOF
 
-def list_patient(body,name)
+def list_patient(body)
   root = Crack::XML.parse(body)
-  result = root["xmlio2"]["patientlst1res"]["Api_Result"]
+  result = root["xmlio2"]["patientlst2res"]["Api_Result"]
   unless result == "00"
     puts "eroor:#{result}"
     exit 1
   end
 
-  pinfo = root["xmlio2"]["patientlst1res"]["Patient_Information"]
+  pinfo = root["xmlio2"]["patientlst2res"]["Patient_Information"]
   pinfo.each do |patient|
-    name2 = patient["WholeName"]
-    if patient["WholeName"] == name
-      puts "ID:      #{patient["Patient_ID"]}"
-      puts "氏名:    #{patient["WholeName"]}"
-      puts "カナ:    #{patient["WholeName_inKana"]}"
-      puts "生年月日:#{patient["BirthDate"]}"
-      if patient["Sex"] == "1"
-        patient["Sex"] = "男"
-      else
-        patient["Sex"] = "女"
-      end
-      puts "性別:    #{patient["Sex"]}"
-      puts "作成日: #{patient["CreateDate"]}"
-      puts "更新日: #{patient["UpdateDate"]}"
+    puts "=========="
+    puts "ID:      #{patient["Patient_ID"]}"
+    puts "氏名:    #{patient["WholeName"]}"
+    puts "カナ:    #{patient["WholeName_inKana"]}"
+    puts "生年月日:#{patient["BirthDate"]}"
+    if patient["Sex"] == "1"
+      patient["Sex"] = "男"
+    else
+      patient["Sex"] = "女"
     end
+    puts "性別:    #{patient["Sex"]}"
+    puts "=========="
+    puts
   end
 end
 
@@ -71,5 +68,5 @@ req.basic_auth(USER, PASSWD)
 
 Net::HTTP.start(HOST, PORT) {|http|
   res = http.request(req)
-  list_patient(res.body,name)
+  list_patient(res.body)
 }
